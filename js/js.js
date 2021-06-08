256bb8f919f5792a592241cf75ed2c290ebd232b
otaviov <amandavv32@gmail.com>
2021-06-08 20:19:17 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -10,6 +10,23 @@ function start() { // Inicio da função start()
 	//Principais variáveis do jogo
 	
 	var jogo = {};
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+		}
+	
+		jogo.pressionou = [];
+		
+		//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+		});
+		
+		$(document).keyup(function(e){
+		   jogo.pressionou[e.which] = false;
+		});
 	
 	//Game Loop
 
@@ -18,6 +35,7 @@ function start() { // Inicio da função start()
 	function loop() {
 	
 	movefundo();
+	movejogador();
 	
 	} // Fim da função loop()
 
@@ -26,8 +44,28 @@ function start() { // Inicio da função start()
 	function movefundo() {
 		esquerda = parseInt($("#fundoGame").css("background-position"));
 		$("#fundoGame").css("background-position",esquerda-2);
+	} // fim da função movefundo()
+	
+	function movejogador() {
+	
+		if (jogo.pressionou[TECLA.W]) {
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo-10);
+		}
+			
+		if (jogo.pressionou[TECLA.S]) {
+				
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);	
+		}
+			
+		if (jogo.pressionou[TECLA.D]) {
+				
+				//Chama função Disparo	
+
+			}
 		
-		} // fim da função movefundo()
+			} // fim da função movejogador()
 	
 
 } //Fim da função start
