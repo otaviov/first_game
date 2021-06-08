da169ac41cd0f5c06114d25e53c626ad9214b7dc
otaviov <amandavv32@gmail.com>
2021-06-08 20:30:16 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -10,6 +10,8 @@ function start() { // Inicio da função start()
 	//Principais variáveis do jogo
 	
 	var jogo = {};
+	var velocidade=5;
+	var posicaoY = parseInt(Math.random() * 334);
 	var TECLA = {
 		W: 87,
 		S: 83,
@@ -33,11 +35,13 @@ function start() { // Inicio da função start()
 
 	jogo.timer = setInterval(loop,30);
 	
+	
 	function loop() {
 	
 	movefundo();
 	movejogador();
-	
+	moveinimigo1();
+
 	} // Fim da função loop()
 
 	//Função que movimenta o fundo do jogo
@@ -73,11 +77,24 @@ function start() { // Inicio da função start()
 		if (jogo.pressionou[TECLA.D]) {
 				
 				//Chama função Disparo	
-
 			}
-		
-			} // fim da função movejogador()
-	
 
+		} // fim da função movejogador()
+
+		function moveinimigo1() {
+
+			posicaoX = parseInt($("#inimigo1").css("left"));
+			$("#inimigo1").css("left",posicaoX-velocidade);
+			$("#inimigo1").css("top",posicaoY);
+				
+				if (posicaoX<=0) {
+				posicaoY = parseInt(Math.random() * 334);
+				$("#inimigo1").css("left",694);
+				$("#inimigo1").css("top",posicaoY);
+					
+				}
+		} //Fim da função moveinimigo1()
+		
+		
 } //Fim da função start
 
