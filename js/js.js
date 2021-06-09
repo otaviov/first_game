4987480390d496fe3db9aa0df32f1cd8bb850e63
otaviov <amandavv32@gmail.com>
2021-06-09 18:41:14 -0300

    pt15

--- js/js.js
+++ js/js.js
@@ -9,6 +9,7 @@ function start() { // Inicio da função start()
  
 	//Principais variáveis do jogo
 	
+	var podeAtirar=true;
 	var jogo = {};
 	var velocidade=5;
 	var posicaoY = parseInt(Math.random() * 334);
@@ -43,6 +44,7 @@ function start() { // Inicio da função start()
 	moveinimigo1();
 	moveinimigo2();
 	moveamigo();
+	colisao();
 
 	} // Fim da função loop()
 
@@ -77,8 +79,9 @@ function start() { // Inicio da função start()
 		}
 			
 		if (jogo.pressionou[TECLA.D]) {
-				
-				//Chama função Disparo	
+
+			disparo();
+
 			}
 
 		} // fim da função movejogador()
@@ -120,6 +123,54 @@ function start() { // Inicio da função start()
 		}
 
 	}
+
+	function disparo() {
+	
+		if (podeAtirar==true) {
+			
+		podeAtirar=false;
+		
+		topo = parseInt($("#jogador").css("top"))
+		posicaoX= parseInt($("#jogador").css("left"))
+		tiroX = posicaoX + 190;
+		topoTiro=topo+37;
+		$("#fundoGame").append("<div id='disparo'></div");
+		$("#disparo").css("top",topoTiro);
+		$("#disparo").css("left",tiroX);
+		
+		var tempoDisparo=window.setInterval(executaDisparo, 30);
+		
+		} //Fecha podeAtirar
+	 
+			   function executaDisparo() {
+			posicaoX = parseInt($("#disparo").css("left"));
+			$("#disparo").css("left",posicaoX+25); 
+	
+					if (posicaoX>900) {
+							
+				window.clearInterval(tempoDisparo);
+				tempoDisparo=null;
+				$("#disparo").remove();
+				podeAtirar=true;
+						
+					   }
+		} // Fecha executaDisparo()
+	} // Fecha disparo()
+
+	function colisao() {
+		var colisao1 = ($("#jogador").collision($("#inimigo1")));
+		// jogador com o inimigo1
+			
+			if (colisao1.length>0) {
+				
+			
+		
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left",694);
+			$("#inimigo1").css("top",posicaoY);
+			}
+		
+		} //Fim da função colisao()
 
 } //Fim da função start
 
